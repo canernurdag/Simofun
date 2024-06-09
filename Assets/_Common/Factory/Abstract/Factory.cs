7d205ea27e33c2ad743f9286e90749f78a8be509
Osman Caner Nurdag <osmancanernurdag@gmail.com>
2024-06-09 22:32:39 +0300

    +Implement: Factory abstract class in case of future needs

---
+++ Assets/_Common/Factory/Abstract/Factory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Simofun.DevCaseStudy.Unity
+{
+    public abstract class Factory
+    {
+       public abstract IProduct CreateIProduct();
+    }
+}
