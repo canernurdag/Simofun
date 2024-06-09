ca08acadec8aaedcf42ebb589d29a1faf9cfeb7c
Osman Caner Nurdag <osmancanernurdag@gmail.com>
2024-06-10 00:41:21 +0300

    +Refactor: Delete unused code

--- Assets/TapDragToClean/Scripts/Runtime/Actors/Cleaner.cs
+++ Assets/TapDragToClean/Scripts/Runtime/Actors/Cleaner.cs
@@ -6,16 +6,6 @@ namespace Simofun.DevCaseStudy.Unity
 {
     public class Cleaner : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+       
     }
 }
