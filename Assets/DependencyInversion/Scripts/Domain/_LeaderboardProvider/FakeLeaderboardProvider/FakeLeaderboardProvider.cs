bb25532880b573d9ce559f0fb402ab03728a4e75
Osman Caner Nurdag <osmancanernurdag@gmail.com>
2024-06-09 22:14:12 +0300

    +Restructure: Case 1 => Folder structure

--- Assets/DependencyInversion/Scripts/Domain/_LeaderboardProvider/FakeLeaderboardProvider/FakeLeaderboardProvider.cs
+++ Assets/DependencyInversion/Scripts/Domain/_LeaderboardProvider/FakeLeaderboardProvider/FakeLeaderboardProvider.cs
@@ -2,18 +2,21 @@
 {
 	using Simofun.DevCaseStudy.Unity.DependencyInversion.Domain.Model;
 	using System.Collections.Generic;
+	using UnityEditor.IMGUI.Controls;
 
 	public class FakeLeaderboardProvider : ILeaderboardProvider
 	{
 		#region Fields
 		const int count = 10;
-		#endregion
+        #endregion
 
+  
         #region Public Methods
         public IEnumerable<ILeaderboardItem> GetItems()
 		{
 			for (var i = 1; i <= count; i++)
 			{
+				
 				yield return new LeaderboardItem
 				{
 					Name = "Name " + i,
